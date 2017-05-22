7f557355f8cd8f40521c301d4357fa16f1fdc23d
Jon Coello <joncoello@gmail.com>
2017-05-22 17:11:28 +0100

    wire up create holiday to db

--- Absence.API/Controllers/HolidayController.cs
+++ Absence.API/Controllers/HolidayController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Absence.API.Controllers
@@ -13,8 +14,19 @@ namespace Absence.API.Controllers
     public class HolidayController : ApiController
     {
         [Route("")]
-        public IHttpActionResult Post([FromBody]HolidayRequest holidayRequest)
+        public async Task<IHttpActionResult> Post([FromBody]HolidayRequest holidayRequest)
         {
+
+            var parts = holidayRequest.DateRange.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var absence = new Models.Absence() {
+                StartDate = DateTime.Parse(parts[0]),
+                EndDate = DateTime.Parse(parts[1])
+            };
+
+            var absenceRepository = new Repositories.AbsenceRepository();
+            await absenceRepository.Create(absence);
+
             return Ok();
         }
     }
---
+++ Absence.API/Repositories/AbsenceRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Absence.API.Repositories
+{
+    public class AbsenceRepository
+    {
+        public async Task Create(Models.Absence absence) {
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["main"].ConnectionString))
+            {
+                await conn.OpenAsync();
+
+                using (var cmd = new SqlCommand("Absence.AbsenceRequest_Create", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@StartDate", absence.StartDate));
+                    cmd.Parameters.Add(new SqlParameter("@EndDate", absence.EndDate));
+
+                    await cmd.ExecuteNonQueryAsync();
+
+                }
+
+            }
+
+        }
+    }
+}
