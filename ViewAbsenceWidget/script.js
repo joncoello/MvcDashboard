09e58b8f9f7946ec6f2ad82a9d7711088e0cc43d
Jon Coello <joncoello@gmail.com>
2017-05-22 21:03:10 +0100

    add calendar to widget

---
+++ ViewAbsenceWidget/script.js
@@ -0,0 +1,52 @@
+﻿(function () {
+
+    var year = 2017;
+
+    var days = [
+        'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+    ];
+
+    var months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    $('#myAbsence').append('<table>');
+    $('#myAbsence > table').addClass('table table-bordered');
+    $('#myAbsence > table').append('<thead>');
+
+    $('#myAbsence > table > thead').append('<tr>');
+    $('#myAbsence > table > thead > tr').append('<th>Month');
+    for (var w = 0; w < 6; w++) {
+        for (var i = 0; i < 7; i++) {
+            $('#myAbsence > table > thead > tr').append('<th>' + days[i]);
+        }
+    }
+
+    $('#myAbsence > table').append('<tbody>');
+
+    for (var i = 0; i < 12; i++) {
+        $('#myAbsence > table > thead').append('<tr>');
+        $('#myAbsence > table > thead > tr').last().append('<td>' + months[i]);
+
+        // first day of month
+        var startDate = new Date(year + '-' + (i + 1) + '-1');
+
+        var startDay = startDate.getDay() == 0 ? 7 : startDate.getDay() + 1;
+
+        for (var d = 0; d < (startDay - 1) ; d++) {
+            $('#myAbsence > table > thead > tr').last().append('<td class="nonday">');
+        }
+
+        for (var d = 0; d < new Date(year, (i + 1), 0).getDate(); d++) {
+            $('#myAbsence > table > thead > tr').last().append('<td>' + (d + 1));
+        }
+
+        var remaining = 42 - (new Date(year, (i + 1), 0).getDate()) - (startDay - 1);
+
+        for (var d = 0; d < remaining ; d++) {
+            $('#myAbsence > table > thead > tr').last().append('<td class="nonday">');
+        }
+
+    }
+
+})();
