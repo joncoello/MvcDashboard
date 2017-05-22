2ebbc7b5afc8cd3ee9417b58ed1ab1a6987e4895
Jon Coello <joncoello@gmail.com>
2017-05-22 21:19:32 +0100

    format calendar

---
+++ ViewAbsenceWidget/WidgetDefinition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewAbsenceWidget
+{
+    public class WidgetDefinition : MvcDashboard.Contracts.IWidget
+    {
+        public string GetHtml()
+        {
+            return ViewAbsenceWidget.Properties.Resources.index;
+        }
+
+        public string GetScript()
+        {
+            return ViewAbsenceWidget.Properties.Resources.script;
+        }
+
+        public List<string> GetScriptReferences()
+        {
+            var refs = new List<string>();
+            return refs;
+        }
+
+        public string GetStyle()
+        {
+            return ViewAbsenceWidget.Properties.Resources.style;
+        }
+
+        public List<string> GetStyleReferences()
+        {
+            return new List<string>();
+        }
+    }
+}
