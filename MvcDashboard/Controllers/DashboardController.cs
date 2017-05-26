a3856e92aca8e254ac4d6491db34c14d5b8cb644
Jon Coello <joncoello@gmail.com>
2017-05-26 16:19:22 +0100

    add web api

---
+++ MvcDashboard/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcDashboard.Controllers
+{
+    public class DashboardController : ApiController
+    {
+        public string Get() {
+            return "hello";
+        }
+    }
+}
