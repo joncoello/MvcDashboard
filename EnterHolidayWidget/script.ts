aca5543d6350445848d2a122e715032edadaea16
Jon Coello <joncoello@gmail.com>
2017-05-29 22:15:35 +0100

    add typescript to enter holiday widget

--- EnterHolidayWidget/script.ts
+++ EnterHolidayWidget/script.ts
@@ -1,15 +1,20 @@
-var widgetElement = document.getElementById('this');
-var widget = new WidgetComponent(widgetElement, {
-    loadData: function (e) {
+﻿var widgetElement: Element = document.getElementById('this');
+var widget: WidgetComponent = new WidgetComponent(widgetElement, {
+    loadData: (e: Element) => {
+
         var dateInput = $('#this >> input[name="daterange"]');
+
         dateInput.daterangepicker({
             locale: {
                 format: 'DD/MM/YYYY'
             }
         });
+
         $('#this > button').click(function () {
-            $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() });
+            $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() })
         });
+
     }
 });
+
 WidgetManager.Instance.registerWidget(widget);
