c2ce64f0909048ab7cb4ceacf559c2b7de3a52ca
Jon Coello <joncoello@gmail.com>
2017-05-29 22:45:35 +0100

    add comments to contract

--- MvcDashboard.Contracts/IWidget.cs
+++ MvcDashboard.Contracts/IWidget.cs
@@ -6,17 +6,52 @@ using System.Threading.Tasks;
 
 namespace MvcDashboard.Contracts
 {
+    /// <summary>
+    /// Contract for defining a dashboard widget to retrieve html, scripts and styling required
+    /// </summary>
     public interface IWidget
     {
+        /// <summary>
+        /// get html of widget
+        /// </summary>
+        /// <returns></returns>
         string GetHtml();
+
+        /// <summary>
+        /// get javascript for widget
+        /// </summary>
+        /// <returns></returns>
         string GetScript();
+
+        /// <summary>
+        /// get css for widget
+        /// </summary>
+        /// <returns></returns>
         string GetStyle();
+
+        /// <summary>
+        /// get any external script references required
+        /// </summary>
+        /// <returns></returns>
         List<string> GetScriptReferences();
+
+        /// <summary>
+        /// get any external style references required
+        /// </summary>
+        /// <returns></returns>
         List<string> GetStyleReferences();
 
+        /// <summary>
+        /// Default layout settings - optional
+        /// </summary>
+        /// <returns></returns>
         WidgetLayout Getlayout();
-        bool HasAuthority();
-        //bool HasAuthority(SecurityManager securityManager);
 
+        /// <summary>
+        /// Security for widget - return false to lock current user
+        /// </summary>
+        /// <returns></returns>
+        bool HasAuthority();
+        
     }
 }
