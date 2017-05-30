fc58ca68027514958655d95e3d2ebfc2261af1f2
Jon Coello <joncoello@gmail.com>
2017-05-30 20:04:14 +0100

    move add handle outside of load data

--- EnterHolidayWidget/script.js
+++ EnterHolidayWidget/script.js
@@ -1,21 +1,23 @@
 (function () {
+    var dateInput = $('#this >> input[name="daterange"]');
+    dateInput.daterangepicker({
+        locale: {
+            format: 'DD/MM/YYYY'
+        }
+    });
+    $('#this > button').click(function () {
+        $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() }, function () {
+            WidgetManager.Instance.refreshWidgets();
+        });
+    });
     var widgetElement = document.getElementById('this');
     var widget = new WidgetComponent(widgetElement, {
         loadData: function (e) {
-            var dateInput = $('#this >> input[name="daterange"]');
-            dateInput.daterangepicker({
-                locale: {
-                    format: 'DD/MM/YYYY'
-                }
-            });
-            $('#this > button').click(function () {
-                $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() }, function () {
-                    WidgetManager.Instance.refreshWidgets();
-                });
-            });
         },
-        saveCustomisation: function (customisation) { },
-        restoreCustomisation: function (customisation) { }
+        saveCustomisation: function (customisation) {
+        },
+        restoreCustomisation: function (customisation) {
+        }
     });
     WidgetManager.Instance.registerWidget(widget);
 })();
--- EnterHolidayWidget/script.ts
+++ EnterHolidayWidget/script.ts
@@ -1,26 +1,30 @@
 ﻿(function () {
 
+    var dateInput = $('#this >> input[name="daterange"]');
+
+    dateInput.daterangepicker({
+        locale: {
+            format: 'DD/MM/YYYY'
+        }
+    });
+
+    $('#this > button').click(function () {
+        $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() }, () => {
+            WidgetManager.Instance.refreshWidgets();
+        })
+    });
+
     var widgetElement: Element = document.getElementById('this');
     var widget: WidgetComponent = new WidgetComponent(widgetElement, {
         loadData: (e: Element) => {
-
-            var dateInput = $('#this >> input[name="daterange"]');
-
-            dateInput.daterangepicker({
-                locale: {
-                    format: 'DD/MM/YYYY'
-                }
-            });
-
-            $('#this > button').click(function () {
-                $.post('http://localhost:1187/api/holiday', { dateRange: dateInput.val() }, () => {
-                    WidgetManager.Instance.refreshWidgets();
-                })
-            });
+            
+        },
+        saveCustomisation: (customisation: { [id: string]: any }) => {
 
         },
-        saveCustomisation: (customisation: { [id: string]: any }) => { },
-        restoreCustomisation: (customisation: { [id: string]: any }) => { }
+        restoreCustomisation: (customisation: { [id: string]: any }) => {
+
+        }
     });
 
     WidgetManager.Instance.registerWidget(widget);
